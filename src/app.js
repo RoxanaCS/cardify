529965d3b27ac86b5255db46c76c955bd994c094
RoxanaCS <roxale.cares@gmail.com>
2018-01-24 20:57:31 -0300

    implementación de la función principal

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-  var attrAlt = $(this).attr('alt');
-  console.log(attrAlt);
 });
-$('.imgHover').mouseover(function() {
-
-});
+cardify();
+// .imgHover es la clase que debe tener el contenedor que tenga las imágenes
+function cardify() {
+  // Buscar todas las imagenes en el contenedor
+  let foundImg = $('.imgHover').find('img');
+  // envuelvo la imagen en un elemento figure
+  foundImg.wrap('<figure style="position: relative"></figure>');
+  // agrego el figcaption pero vacio
+  $('figure').append('<figcaption style="position: absolute; margin: 5em 5em; font-size: 150%; color: #FFF; text-align: center; display: block"></figcaption>');
+  foundImg.mouseover(function() {
+    // obtener el alt de la images y guardarlo
+    let attrAlt = $(this).attr('alt');
+    // hacer que la imagen se oscurezca
+    let newImg = $(this).css('filter', 'brightness(0.1)');
+    // obtener el src de la imagen
+    let src = newImg.attr('src');
+    // muestre el contenido del alt en el figcaption
+    return newImg + $(this).siblings('figcaption').append(attrAlt);
+  });
+  // evento para que desapareza el efecto anterior
+  foundImg.mouseout(function() {
+    $(this).css('filter', 'brightness(1)');
+    $('figcaption').html('');
+  });
+}
