fc5cf057f5f6baccd04bcbe9ffaa81537fe470d1
AGuerrav <aaguerravaldes@gmail.com>
2018-01-26 11:26:48 -0300

    test

--- test/test.js
+++ test/test.js
@@ -1,1 +1,15 @@
-chuchuchcuhcucgua
+const xxx = require('../src/app'); // exportamos la funcion
+let assert = require('assert');
+let expect = require('chai').expect;
+
+describe('Cardify', function() { // indica el modulo que vamos a testear
+  it('Should be a .jgp image ', function() {
+
+    var perro = document.getElementsByTagName('img');
+    var src = perro.attr('src');
+    console.log(src);
+    var img = '';
+    $(img).src.val = gato
+    document.getElementById('myPhoto').src=img;
+    expect( img).to.equal('.jpg');
+  });
