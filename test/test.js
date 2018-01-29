4400c6d5e7b1433b52fb3c9a7001d71faf73249a
RoxanaCS <roxale.cares@gmail.com>
2018-01-29 20:00:13 -0300

    tercer test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-let dom = new JSDOM('<!DOCTYPE html><html><body><div class="containerFunction"><img src="https://cdn.pixabay.com/photo/2017/08/05/23/37/beautiful-foot-boletus-2586702_960_720.jpg"></></div></body></html>');
+let dom = new JSDOM('<!DOCTYPE html><html><body><div class="containerFunction"><img src="https://cdn.pixabay.com/photo/2017/08/05/23/37/beautiful-foot-boletus-2586702_960_720.jpg" alt="Beautiful-foot-boletus"></></div></body></html>');
 const { window } = dom;
 global.document = dom;
 global.window = window;
@@ -15,14 +15,17 @@ const cardify = require('../src/app');
 describe('Función cardify', () => {
   // ejecuto la función con el html que se creo mas arriba
   $('.containerFunction').cardify;
-  // pruebo si efectivamente la función general el elemento figure al ejetutarse
+
   it('Se crea el elemento figure', () => {
     chai.assert.exists($('figure'));
   });
-});
-describe('Función cardify', () => {
-  $('.containerFunction').cardify;
+
   it('Se crea el elemento figcaption', () => {
     chai.assert.exists($('figcaption'));
+  });
+
+  it('figcaption no está vacío', () => {
+    let lengthFigcaption = ($('figcaption').length);
+    chai.expect($('figcaption')).to.have.length(lengthFigcaption);
   });
 });
